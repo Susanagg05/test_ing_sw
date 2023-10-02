222c588fd5e70b85e6c662c70177eb30adc72096
Susanagg05 <susiguga2001@gmail.com>
2023-10-02 13:39:05 -0500

    feat: Login Page bgcolor

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -6,7 +6,29 @@ class LoginPage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Image.asset("assets/meme.jpeg"),
+    backgroundColor: const Color(0xff131E40) ,
+      body:Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+        const Spacer(),
+        const Text (
+          "Te damos la bienvenida a NeuroSight",
+          textAlign: TextAlign.start,
+          style: TextStyle(
+            fontSize: 30
+          )
+          ),
+        const Spacer(),
+        TextFormField(),
+        const Spacer(),
+        Image.asset("assets/meme.jpeg"),
+        const SizedBox(
+          height: 60,
+          width: 120,
+          child: InkWell(),
+        )
+      ]
+    )
     );
   } 
 }
