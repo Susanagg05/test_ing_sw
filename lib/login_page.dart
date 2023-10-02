7a00bff1a295dcf7204907fb7a9b305a6b934ce4
Susanagg05 <susiguga2001@gmail.com>
2023-10-02 13:19:03 -0500

    feat: agregar imagen de inicio

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -6,7 +6,7 @@ class LoginPage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Algo"),
+      body: Image.asset("assets/meme.jpeg"),
     );
   } 
 }
