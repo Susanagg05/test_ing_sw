755c950c4653e3be1f45233a11134b09c715d822
Andrés Arias Medina <77759820+AndresAriasM@users.noreply.github.com>
2023-10-19 02:16:10 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,15 @@
 import 'package:flutter/material.dart';
+import 'menu.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: const Icon(Icons.add),
-        ), 
-        appBar: AppBar(
-          title: const Text("Mi primera App"),
-        ),
-        backgroundColor: const Color(0xff131e40),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text("Has presionado esto"),
-              const Text("x veces"),
-              GestureDetector(
-               onTap: () {},
-               child: const Text ("Presioname"),
-              ),
-              ElevatedButton(
-                onPressed: () {}
-              , child: const Text("Ingresar"))
-            ],
-          ),
-        ),
-      ),
+      home: MenuWidget(),
     );
   }
 }
