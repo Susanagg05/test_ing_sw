bb133478e0dbb2f4bb256b326f386801b049760b
Susanagg05 <susiguga2001@gmail.com>
2023-10-25 13:21:18 -0500

    feat firebase connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:test_ing_mw/firebase_options.dart';
 import 'menu.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(MyApp());
 }
 
